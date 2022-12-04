6b062f004fef83e0bcbdf214363e6ff6a4978400
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-12-04 17:05:26 +0100

    style article preview and i need now to redirect on click

--- src/components/articles/articles_root.vue
+++ src/components/articles/articles_root.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col py-5">
+  <div class="flex flex-col py-5 h-[85%]">
     <div class="w-full">
       <div class="flex justify-end">
 
@@ -14,22 +14,14 @@
         </div>
     </div>
 
-    <div class="w-full flex justify-around mt-10">
-      <div class="bg-red-300 inset-0 transform  hover:rotate-[30deg] hover:scale-105 transition duration-300 flex flex-col items-center py-5 w-1/4 ">
-        <div>title</div>
-        <img :src="dummylogo" class="w-16" alt="">
-        <div>idea</div>
+    <div class="w-full flex flex-wrap justify-around mt-10 h-3/4">
+
+      <div v-for="article in articles" :key="article.id" @click="goToArticle(article.id)" class="cursor-pointer bg-gradient-to-b from-cyan-500 to-blue-500 rounded-lg inset-0 transform  hover:rotate-[30deg] hover:scale-[1.03] transition duration-300 flex flex-col items-center justify-around py-5 w-1/4">
+        <div class="text-white font-bold text-2xl">{{titleTransform(article.title)}}</div>
+        <img :src="article.logo" alt="article logo" class="w-1/4">
+        <div class="text-lg text-white italic text-center">{{shortenLongText(article.idea)}}</div>
       </div>
-      <div class="bg-red-300 inset-0 transform  hover:rotate-[30deg] hover:scale-105 transition duration-300 flex flex-col items-center py-5 w-1/4 ">
-        <div>title</div>
-        <img :src="dummylogo" class="w-16" alt="">
-        <div>idea</div>
-      </div>
-      <div class="bg-red-300 inset-0 transform  hover:rotate-[30deg] hover:scale-105 transition duration-300 flex flex-col items-center py-5 w-1/4 ">
-        <div>title</div>
-        <img :src="dummylogo" class="w-16" alt="">
-        <div>idea</div>
-      </div>
+
     </div>
   </div>
 
@@ -37,23 +29,55 @@
   
   <script lang="ts">
   import { defineComponent } from 'vue';
-  
+  import {BasicArticle} from './dto/index';
+
   export default defineComponent({
     name: 'ArticlesCompo',
+    created() {
+      for (let i = 0; i < 3; ++i) {
+        this.addArticle();
+      }
+    },
     data() {
       return {
+        ideaMaxLen: 80,
+        titleMaxLen: 20,
         searchStr: '',
+        max_article_size: 3,
+        articles: [] as Array<BasicArticle>,
       }
     },
     methods: {
       searchForArticle() {
         console.log(`searching for ${this.searchStr}`);
+      },
+      addArticle() {
+        this.articles.push({
+        id:1,
+        title: 'my title uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu',
+        idea: 'Which of the two sovereigns is imbued with the Moral law? (2) Which of the two generals has most ability? (3) With whom lie the advantages derived from Heaven and Earth? (4) On which side is discipline most rigorously enforced? (5) Which army is stronger? (6) On which side are officers and men more highly trained? (7) In which army is there the greater constancy both in reward and punishment?',
+        logo: require("@/assets/user/user-img.jpg"),
+        time: 34
+      });
+      },
+      shortenLongText(value:string) : string {
+        if (value.length > this.ideaMaxLen)
+          return value.slice(0, this.ideaMaxLen) + `...`;
+        else
+          return value;
+      },
+      titleTransform(value:string) : string {
+        const upper = value.charAt(0).toUpperCase() + value.slice(1);
+        if (upper.length > this.titleMaxLen)
+          return upper.slice(0, this.titleMaxLen) + `...`;
+        else
+          return upper;
+      },
+      goToArticle(articleId:number) {
+        console.log(`go to article ${articleId}`);
       }
     },
     computed: {
-      dummylogo() : string {
-        return require("@/assets/user/user-img.jpg");
-      }
     }
   });
   
---
+++ src/components/articles/dto/article.ts
@@ -0,0 +1,7 @@
+export interface    BasicArticle {
+    id:number;
+    title:string;
+    idea: string;
+    logo:string;
+    time:number;
+}
